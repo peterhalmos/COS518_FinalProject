e4e8fdbee40c0f2f5b8149d26feb094231545224
Peter L. Halmos <ph3641@della8.princeton.edu>
2024-04-17 12:18:06 -0400

    added skeleton code for the emulator and classes for DHTs

---
+++ Chord.py
@@ -0,0 +1,26 @@
+
+class Node:
+    
+    def __init__(self, ID):
+        self.id = ID
+        return
+    def find_succ(self):
+        return
+    def find_pred(self):
+        return
+    def closest_preceeding_finger(self):
+        return
+    def join(self):
+        return
+    def init_finger_table(self):
+        return
+    def update_finger_table(self):
+        return
+    def update_others(self):
+        return
+    def stabilize(self):
+        return
+    def notify(self):
+        return
+    def fix_fingers(self):
+        return
---
+++ Koorde.py
@@ -0,0 +1,4 @@
+class Node:
+    pass
+    def lookup():
+        pass
